f271504b78517140228b4458f3ae3874b28760e7
Monir Hussain <monir22205101801@diu.edu.bd>
2026-02-04 17:15:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
 import streamlit as st
 from summarizer import summarize_text
 
-st.set_page_config(page_title="SummarIQ", layout="centered")
+st.set_page_config(
+    page_title="SummarIQ",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+st.markdown("""
+<style>
+.stButton>button {
+    background: #0F3479E4;
+    border-radius: 10px;
+    font-weight: 600;
+}
+.main-title {
+    font-size: 40px;
+    font-weight: 800;
+    background-color: #0F3479E4;
+    border-radius: 20px;
+    padding-top: 0px;
+}
+</style>
+""", unsafe_allow_html=True)
+st.sidebar.header("SUMMARIQ")
+st.sidebar.divider()
+st.sidebar.header("Let you know the summary of the text.")
+st.sidebar.divider()
+st.sidebar.markdown("""
+**Please be patient**
+- More features will be added soon
+                    
+**Developed by**
+- Zakir Hussain Monir
+- Daffodil International University
+""")
 
-st.title("🧠 SummarIQ - Intelligent Text Summarization")
-
+st.markdown("<div class='main-title'>🧠 SummarIQ - Intelligent Text Summarizer</div>", unsafe_allow_html=True)
+st.caption("Powered by BART Transformer Model, HuggingFace Dataset.")
+st.divider()
 text = st.text_area("Enter text to summarize", height=300)
-
 if st.button("Generate Summary"):
     if text.strip():
         summary = summarize_text(text)
